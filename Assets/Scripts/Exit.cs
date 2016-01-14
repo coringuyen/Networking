5097be7dd9bf02e0512e5290109808626988cbd7
coringuyen <coringuyen96@gmail.com>
2016-01-14 14:59:58 -0600

    Networking

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Exit : MonoBehaviour {
+
+    public Button exit;
+	void Start ()
+    {
+        exit.GetComponent<RectTransform>().position = new Vector3(Screen.width * 0.8f, Screen.height * 0.9f, 0);
+
+        exit.gameObject.SetActive(true);	
+	}
+	
+	public void ExitApplication()
+    {
+        Application.Quit();
+    }
+}
